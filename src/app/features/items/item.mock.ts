3c0d3ea961fd480edb6acd9345ee5a2c9fda9c05
Hamza Al Saado <Hsaado@beamra.com>
2026-04-02 02:04:50 +0300

    Add main logo. Add scrollbar to the tables.

--- src/app/features/items/item.mock.ts
+++ src/app/features/items/item.mock.ts
@@ -5,5 +5,15 @@ export const ITEMS_MOCK: Item[] = [
   { id: 2, name: 'سويتش', category: 'شبكة', department: 'تكنولوجيا المعلومات', quantity: 3 },
   { id: 3, name: 'ربيتر', category: 'لاسلكي', department: 'تكنولوجيا المعلومات', quantity: 7 },
   { id: 4, name: 'طابعة Canon', category: 'مكتبية', department: 'الإدارة', quantity: 2 },
+  { id: 5, name: 'لابتوب Asus', category: 'إلكترونيات', department: 'تكنولوجيا المعلومات', quantity: 4 },
+  { id: 1, name: 'راوتر Cisco', category: 'شبكة', department: 'تكنولوجيا المعلومات', quantity: 5 },
+  { id: 2, name: 'سويتش', category: 'شبكة', department: 'تكنولوجيا المعلومات', quantity: 3 },
+  { id: 3, name: 'ربيتر', category: 'لاسلكي', department: 'تكنولوجيا المعلومات', quantity: 7 },
+  { id: 4, name: 'طابعة Canon', category: 'مكتبية', department: 'الإدارة', quantity: 2 },
+  { id: 5, name: 'لابتوب Asus', category: 'إلكترونيات', department: 'تكنولوجيا المعلومات', quantity: 4 },
+  { id: 1, name: 'راوتر Cisco', category: 'شبكة', department: 'تكنولوجيا المعلومات', quantity: 5 },
+  { id: 2, name: 'سويتش', category: 'شبكة', department: 'تكنولوجيا المعلومات', quantity: 3 },
+  { id: 3, name: 'ربيتر', category: 'لاسلكي', department: 'تكنولوجيا المعلومات', quantity: 7 },
+  { id: 4, name: 'طابعة Canon', category: 'مكتبية', department: 'الإدارة', quantity: 2 },
   { id: 5, name: 'لابتوب Asus', category: 'إلكترونيات', department: 'تكنولوجيا المعلومات', quantity: 4 }
 ];
